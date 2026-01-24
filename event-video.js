f579e812e0b32c2be12b5b5d6d73c380eab1dbcb
Anay Mishra <anay24@iisertvm.ac.in>
2026-01-24 11:37:17 +0530

    Create event-video.js

---
+++ event-video.js
@@ -0,0 +1,12 @@
+const video = document.querySelector(".event-video");
+
+if (video) {
+  document.addEventListener("scroll", () => {
+    const rect = video.getBoundingClientRect();
+    const visible = rect.top < window.innerHeight && rect.bottom > 0;
+
+    if (!visible && !video.paused) {
+      video.pause();
+    }
+  });
+}
